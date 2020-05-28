e71e840f7c13ec3e9e566f14379755a45be249da
shiliangl <1462999406@qq.com>
2020-05-28 11:23:30 +0800

    指令整理修改

---
+++ src/directive/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: shiliangL
+ * @Date: 2020-05-28 11:22:52
+ * @LastEditTime: 2020-05-28 11:22:53
+ * @LastEditors: Do not edit
+ * @Description:
+ * @FilePath: /topevery-ui-admin/src/directive/index.js
+ */
